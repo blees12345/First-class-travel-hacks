28668b43b48225814791b1a48861ca1de4d03b3c
Adrian Guia <adriang1702@gmail.com>
2022-11-28 21:36:04 -0800

    added routes for home page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import './App.css';
 import BlogList from './BlogList';
 import SingleBlog from './SingleBlog';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar,} from 'react-bootstrap';
 function App() {
 	return (
-		<div >
-			<NavBar></NavBar>
+		<div>
 			<Router>
+				<Navbar collapseOnSelect expand='lg' variant='dark'>
+					<Link to='/'>
+						<Navbar.Brand href='#home'>First Class Travel Hacks</Navbar.Brand>
+					</Link>
+
+					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+					<Navbar.Collapse id='responsive-navbar-nav'>
+						<Nav className='me-auto'>
+							<Nav.Link href='#miles&redemption'>
+								Points & Miles Redemption
+							</Nav.Link>
+							<Nav.Link href='#creditcards'>Credit Cards</Nav.Link>
+							<Nav.Link href='#'>Travel Hack Intro</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
 				<Routes>
-					<Route path='/blogDetails/:id' element={<SingleBlog />} />
-					<Route path='/blogList' element={<BlogList />} />
-					<Route path='/' element={<BlogList />} />
+					
+						 <Route path='/blogDetails/:id' element={<SingleBlog />} />
+						<Route path='/blogList' element={<BlogList />} />
+						 <Route path='/' element={<BlogList />} />
+						{' '}
+					
 				</Routes>
+				
 			</Router>
 		</div>
+		// <Router>
+		// 			<Routes>
+
+		// 				<Route path='/blogDetails/:id' element={<SingleBlog />} />
+		// 				<Route path='/blogList' element={<BlogList />} />
+		// 				<Route path='/' element={<BlogList />} />
+		// 			</Routes>
+		// 		</Router>
 	);
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,23 +1,28 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Nav, Navbar, NavLink} from 'react-bootstrap';
+import { BrowserRouter as  Router,Link,Routes,Route} from 'react-router-dom';
 
 function CollapsibleExample() {
 	return (
-		<Navbar collapseOnSelect expand='lg' variant='dark'>
-			<Container>
-				<Navbar.Brand href='#home'>First Class Travel Hacks</Navbar.Brand>
+		<Router>
+			<Navbar collapseOnSelect expand='lg' variant='dark'>
+				<Link to='/'>
+					<Navbar.Brand href='#home'>First Class Travel Hacks</Navbar.Brand>
+				</Link>
+
 				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 				<Navbar.Collapse id='responsive-navbar-nav'>
 					<Nav className='me-auto'>
-						<Nav.Link href='#features'>Points & Miles Redemption</Nav.Link>
-						<Nav.Link href='#pricing'>Credit Cards</Nav.Link>
-						<Nav.Link href='#pricing'>Travel Hack Intro</Nav.Link>
+						<Nav.Link href='#miles&redemption'>Points & Miles Redemption</Nav.Link>
+						<Nav.Link href='#creditcards'>Credit Cards</Nav.Link>
+						<Nav.Link href='#'>Travel Hack Intro</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
-			</Container>
-		</Navbar>
+			</Navbar>
+			<Routes>
+				<Route path='/' ></Route>
+			</Routes>
+		</Router>
 	);
 }
 
