df71a85a6a6d196f20b25cd22c577999a531eab9
Adrian Guia <adriang1702@gmail.com>
2022-12-12 16:50:47 -0800

    changed background color and nav link font

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import EarningMiles from './EarningMiles';
 import { HashRouter as Router, Route, Routes,Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar,} from 'react-bootstrap';
-import vacation from './vacation.jpg';
-import HomeModal from './HomeModal';
+
 function App() {
 	return (
 		<div className='main'>
 			<Router>
-				<Navbar collapseOnSelect expand='lg' variant='dark'>
+				<Navbar collapseOnSelect expand='lg' className='navbar' >
 					<Link to='/'>
 						<Navbar.Brand href='#home'>First Class Travel Hacks</Navbar.Brand>
 					</Link>
@@ -20,15 +19,15 @@ function App() {
 					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 					<Navbar.Collapse id='responsive-navbar-nav'>
 						<Nav  className='navLinks'>
-							<Nav.Link href='blogDetails/6Eow2G8YqMfSUfmJqGDyYC#/blogDetails/6Eow2G8YqMfSUfmJqGDyYC'>
+							<Nav.Link className='navlink' href='blogDetails/6Eow2G8YqMfSUfmJqGDyYC#/blogDetails/6Eow2G8YqMfSUfmJqGDyYC'>
 								Points And Miles Guide
 							</Nav.Link>
 
-							<Nav.Link href='blogDetails/3mLKIIfhloEtK1ySi9zit8#/blogDetails/3mLKIIfhloEtK1ySi9zit8'>
+							<Nav.Link className='navlink' href='blogDetails/3mLKIIfhloEtK1ySi9zit8#/blogDetails/3mLKIIfhloEtK1ySi9zit8'>
 								Earning Points & Miles
 							</Nav.Link>
-							<Nav.Link href='/creditcards'>Credit Cards</Nav.Link>
-							<Nav.Link href='/miles&redemption'>
+							<Nav.Link className='navlink' href='/creditcards'>Credit Cards</Nav.Link>
+							<Nav.Link className='navlink' href='/miles&redemption'>
 								Points & Miles Redemption
 							</Nav.Link>
 						</Nav>
